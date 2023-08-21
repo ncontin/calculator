443aed8dfc9e6ffff514ef184bfcb09fce4a5672
Nicolò Contin <95549154+nicolo-contin@users.noreply.github.com>
2023-08-21 22:25:48 +0200

    make calculator work with two numbers

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let firstNum = 0;
 let secondNum = 0;
 let operator = "";
 
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+
+let enteredDigits = [];
+let displayedNumber = display.value;
+
 function add(a, b) {
     return a + b;
 }
@@ -28,10 +37,37 @@ function divide(...numbers) {
 
 function operate(operator, firstNum, secondNum) {
     if (operator === "+") return add(firstNum, secondNum);
-    if (operator === "-") return add(firstNum, secondNum);
-    if (operator === "*") return add(firstNum, secondNum);
-    if (operator === "/") return add(firstNum, secondNum);
+    if (operator === "-") return subtract(firstNum, secondNum);
+    if (operator === "X") return multiply(firstNum, secondNum);
+    if (operator === "/") return divide(firstNum, secondNum);
     else return "error";
 }
 
-console.log(operate("+", 1, 2))
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        enteredDigits.push(number.textContent);
+        displayedNumber = parseInt((display.value = enteredDigits.join("")));
+    });
+});
+
+operators.forEach((operation) => {
+    operation.addEventListener("click", () => {
+        firstNum = displayedNumber;
+        operator = operation.textContent;
+        enteredDigits = [];
+    });
+});
+
+equal.addEventListener("click", () => {
+    secondNum = displayedNumber;
+    console.log(operator, firstNum, secondNum);
+    display.value = operate(operator, firstNum, secondNum);
+});
+
+clear.addEventListener("click", () => {
+    firstNum = 0;
+    secondNum = 0;
+    operator = "";
+    enteredDigits = [];
+    display.value = 0;
+});
