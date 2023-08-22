f34da1d9999de6991c7e4c64685649005f25a272
Nicolò Contin <95549154+nicolo-contin@users.noreply.github.com>
2023-08-22 16:29:15 +0200

    setup tailwind and add first styling

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+        },
+    },
+    plugins: [],
+};
